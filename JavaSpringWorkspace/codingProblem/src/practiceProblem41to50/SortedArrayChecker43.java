cd58015f7a9a22ce33b05c3773ce66495dbdfa87
AboutSatyam <programmersatyam07@gmail.com>
2026-07-31 14:15:10 +0530

    1

--- JavaSpringWorkspace/codingProblem/src/practiceProblem41to50/SortedArrayChecker43.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem41to50/SortedArrayChecker43.java
@@ -9,6 +9,41 @@ public class SortedArrayChecker43 {
 		System.out.println("Welcome To Sorted Array Checker : ");
 		Scanner Scan = new Scanner(System.in);
 		int[] numArray = ArrayUtility.inputArray();
+		boolean isdec = isDecresing(numArray);
+		boolean isinc = isIncresing(numArray);
+
+		if (isdec || isinc) {
+			System.out.println("This is a Sorted Array");
+		} else {
+			System.out.println("This is not a Sorted Array");
+		}
 	}
 
+	public static boolean isDecresing(int[] numArray) {
+		int i = 1;
+		while (i < numArray.length) {
+
+			if (numArray[i] > numArray[i - 1]) {
+
+				return false;
+			}
+			i++;
+		}
+
+		return true;
+	}
+
+	public static boolean isIncresing(int[] numArray) {
+		int i = 1;
+		while (i < numArray.length) {
+
+			if (numArray[i] < numArray[i - 1]) {
+
+				return false;
+			}
+
+			i++;
+		}
+		return true;
+	}
 }
