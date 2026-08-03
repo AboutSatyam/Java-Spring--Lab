30044eea58ee6a7556fd3f4134a5cde654b8b5be
AboutSatyam <programmersatyam07@gmail.com>
2026-08-03 14:41:17 +0530

    3Aug

---
+++ JavaSpringWorkspace/codingProblem/src/in/advance/challenge77/geometry/Circle.java
@@ -0,0 +1,10 @@
+package in.advance.challenge77.geometry;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- JavaSpringWorkspace/codingProblem/src/in/advance/challenge77/utils/Calculator.java
+++ JavaSpringWorkspace/codingProblem/src/in/advance/challenge77/utils/Calculator.java
@@ -1,6 +1,6 @@
-package in.kgcoding.challenge77.geometry;
+package in.advance.challenge77.utils;
 
-public class Circle {
+public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
