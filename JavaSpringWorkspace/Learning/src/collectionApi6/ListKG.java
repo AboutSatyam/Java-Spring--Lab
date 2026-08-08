c909b1eb331516c8066a109ca84c2e8fd214a69d
AboutSatyam <programmersatyam07@gmail.com>
2026-08-08 15:12:44 +0530

    Update

--- JavaSpringWorkspace/Learning/src/collectionApi6/ListKG.java
+++ JavaSpringWorkspace/Learning/src/collectionApi6/ListKG.java
@@ -18,6 +18,17 @@ public class ListKG {
 		System.out.println(li);
 		Collections.sort(li);
 		System.out.println(li);
+		
+		// List Arr = new ArrayList(); // Without Type Of Data
+		//List<String> Arr = new ArrayList<>(); // With Type Of Of Data Means Generics
+		// List<Integer> Arr = new ArrayList<>(); //Only Wrapper Classes
+
+//		Arr.add("Hello World");
+//		Arr.add(1, "Hello Satyam");
+//		Arr.size();
+		// Arr.add(54);
+
+//		System.out.println(Arr.size());
 
 	}
 
