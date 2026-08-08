2e334bc89ab2fae47719996145a30ab866c8c5f2
AboutSatyam <programmersatyam07@gmail.com>
2026-08-08 21:22:53 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem89to95/PriorityQueue94.java
@@ -0,0 +1,46 @@
+package practiceProblem89to95;
+
+public class PriorityQueue94 {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static class Student {
+		private final String name;
+		private final char grade;
+
+		public Student(String name, char grade) {
+			super();
+			this.name = name;
+			this.grade = grade;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public char getGrade() {
+			return grade;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
