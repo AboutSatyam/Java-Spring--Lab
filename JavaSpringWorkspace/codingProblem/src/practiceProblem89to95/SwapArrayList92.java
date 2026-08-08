788796e8d3f2185ecec0f7d22d4c5becf43962de
AboutSatyam <programmersatyam07@gmail.com>
2026-08-08 20:17:28 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/practiceProblem89to95/SwapArrayList92.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem89to95/SwapArrayList92.java
@@ -1,9 +1,23 @@
 package practiceProblem89to95;
 
+import java.io.ObjectInputStream.GetField;
+import java.util.Arrays;
+import java.util.List;
+
 public class SwapArrayList92 {
 
 	public static void main(String[] args) {
-	
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+		System.out.println(list);
+
+		swap(list, 1, 7);
+		System.out.println(list);
+	}
+
+	public static void swap(List<Integer> list, int x, int y) {
+		int swap = list.get(x);
+		list.set(x, list.get(y));
+		list.set(y, swap);
 	}
 
 }
