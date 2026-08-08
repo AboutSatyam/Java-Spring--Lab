cc494a5d34dd3f604b408756112c939732bcfb21
AboutSatyam <programmersatyam07@gmail.com>
2026-08-08 22:51:48 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem89to98/EnchanceDAYenums97.java
@@ -0,0 +1,30 @@
+package practiceProblem89to98;
+
+public class EnchanceDAYenums97 {
+
+	enum Day {
+		Monday(true),
+		Tuesday(true),
+		Wednesday(true),
+		Thursday(true),
+		Friday(true),
+		Saturday(false),
+		Sunday(false);
+
+		private final boolean isWeekday;
+
+		private Day(boolean isWeekday) {
+			this.isWeekday = isWeekday;
+		}
+
+		public String getType() {
+			return isWeekday ? "WeekDay" : "WeekEnd";
+		}
+	}
+
+	public static void main(String[] args) {
+		for (Day day : Day.values()) {
+			System.out.printf("%s : %s\n", day, day.getType());
+		}
+	}
+}
--- JavaSpringWorkspace/codingProblem/src/practiceProblem89to98/WeekEnums96.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem89to98/WeekEnums96.java
@@ -2,4 +2,18 @@ package practiceProblem89to98;
 
 public class WeekEnums96 {
 
+	enum Day {
+		Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Here IS Your All The Days In A Week ");
+
+		System.out.println(Day.Monday +" With Sysout");
+		for (Day day : Day.values()) {
+			System.out.println(day);
+		}
+
+	}
+
 }
