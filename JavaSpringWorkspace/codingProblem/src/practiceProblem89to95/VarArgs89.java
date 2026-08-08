ab0c47420a9b3a28fa2337642721bc3a38ef5109
AboutSatyam <programmersatyam07@gmail.com>
2026-08-08 11:16:23 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem89to95/VarArgs89.java
@@ -0,0 +1,26 @@
+package practiceProblem89to95;
+
+public class VarArgs89 {
+
+	public static void main(String[] args) {
+		System.out.println(concatenate("Satyam"));
+		System.out.println(concatenate("Amresh", "Kumar"));
+		System.out.println(concatenate("Pandey"));
+	}
+
+	public static String concatenate(String... strs)
+	/**
+	 * "This method can receive any number of String arguments Inside the method,
+	 * strs behaves like a String array
+	 **/
+
+	{
+		StringBuilder sb = new StringBuilder();
+		for (String str : strs) {
+			sb.append(str).append(" ");
+		}
+		return sb.toString();
+
+	}
+
+}
