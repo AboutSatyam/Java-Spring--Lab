adcac86ca6f39d159e8d8fcdb00c38f78d2f07b4
AboutSatyam <programmersatyam07@gmail.com>
2026-08-10 20:53:36 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/StringFilterReduce108.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/StringFilterReduce108.java
@@ -1,9 +1,18 @@
 package practiceProblem106to115;
 
+import java.util.List;
+
 public class StringFilterReduce108 {
 
 	public static void main(String[] args) {
 
+		List<String> strs = List.of("Satyam Pandey", "Done" ,"Hello World", "Java THEN Spring THEN Devops");
+	         String res = strs.stream()
+		       .filter(str -> str.length() > 10)
+		           .reduce("", (a, b) -> a + " " + b);
+	
+	          System.out.println(res);
+		
 	}
 
 }
