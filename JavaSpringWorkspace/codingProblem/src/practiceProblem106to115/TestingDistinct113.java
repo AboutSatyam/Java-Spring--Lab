6b4e4880a3285187bf61bc8762462efd27827656
AboutSatyam <programmersatyam07@gmail.com>
2026-08-11 00:28:25 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/TestingDistinct113.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/TestingDistinct113.java
@@ -3,7 +3,7 @@ package practiceProblem106to115;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TestingDistinct {
+public class TestingDistinct113 {
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(1, 2, 3, 2, 5, 3, 4, 2, 5, 3, 45, 6, 2);
 		List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
