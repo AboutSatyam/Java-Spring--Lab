8bc3523bfb35fb6a1c3682c2290c1e7e2e30719f
AboutSatyam <programmersatyam07@gmail.com>
2026-08-11 01:00:58 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/OperationMap.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/OperationMap.java
@@ -1,9 +1,15 @@
 package practiceProblem106to115;
 
+import java.util.List;
+
 public class OperationMap {
 
 	public static void main(String[] args) {
 
+		List<String> no = List.of("1", "2", "3", "4", "5", "6", "7", "5");
+		no.stream().map(Integer::parseInt).map(num -> Math.pow(num, 2)).reduce((a, b) -> a + b)
+
+				.ifPresent(System.out::println);
 	}
 
 }
