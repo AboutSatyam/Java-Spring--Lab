437e4c79995c2314d7cb5188e4d3438b3f6d3873
AboutSatyam <programmersatyam07@gmail.com>
2026-08-10 23:25:38 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/in/advance/challenge111/Factorial.java
@@ -0,0 +1,5 @@
+package in.advance.challenge111;
+
+public class Factorial {
+
+}
---
+++ JavaSpringWorkspace/codingProblem/src/in/advance/challenge111/TestingFactor.java
@@ -0,0 +1,10 @@
+package in.advance.challenge111;
+
+public class TestingFactor {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
