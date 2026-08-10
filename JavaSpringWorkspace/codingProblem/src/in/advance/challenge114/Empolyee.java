14358ab6fb530a03bee0f13fd48d50d9a3c62700
AboutSatyam <programmersatyam07@gmail.com>
2026-08-11 00:35:37 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/in/advance/challenge114/Empolyee.java
@@ -0,0 +1,21 @@
+package in.advance.challenge114;
+
+public class Empolyee {
+	private final String name;
+	private final int salary;
+
+	public Empolyee(String name, int salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+}
