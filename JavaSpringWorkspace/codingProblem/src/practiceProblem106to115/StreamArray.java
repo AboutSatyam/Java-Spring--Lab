51cfdbb21cb714669fe8984d423eb8404d607211
AboutSatyam <programmersatyam07@gmail.com>
2026-08-10 20:40:08 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/StreamArray.java
+++ JavaSpringWorkspace/codingProblem/src/practiceProblem106to115/StreamArray.java
@@ -7,6 +7,8 @@ public class StreamArray {
 	public static void main(String[] args) {
 
 		List<String> names = List.of("Satyam", "Bhai", "64");
+		names.stream().forEach(name -> System.out.println(name));
+
 	}
 
 }
