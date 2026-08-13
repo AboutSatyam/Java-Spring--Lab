79880ffe64cf77e2f2a8ae7cfbc432812f10a722
AboutSatyam <programmersatyam07@gmail.com>
2026-08-13 19:55:32 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program1/Calculator.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program1/Calculator.java
@@ -42,7 +42,11 @@ public class Calculator {
 			break;
 
 		case 4:
-			System.out.println("Division is : " + (num1 / num2));
+			if (num2 != 0) {
+				System.out.println("Division is : " + (num1 / num2));
+			} else {
+				System.out.println("Cannot Divide By Zero !!");
+			}
 			break;
 
 		default:
