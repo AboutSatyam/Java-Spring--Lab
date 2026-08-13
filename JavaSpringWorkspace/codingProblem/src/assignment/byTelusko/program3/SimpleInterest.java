ce07098ccfadda3a977046ab3a7b322bfa542859
AboutSatyam <programmersatyam07@gmail.com>
2026-08-13 21:55:48 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program3/SimpleInterest.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program3/SimpleInterest.java
@@ -1,8 +1,28 @@
 package assignment.byTelusko.program3;
 
+import java.util.Scanner;
+
 public class SimpleInterest {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome To Simple Interest Calculator ");
+		Scanner Scan = new Scanner(System.in);
+		double principal;
+		double interest;
+		double timePeriod;
+
+		System.out.println("Enter The Principal Amount : ");
+		principal = Scan.nextDouble();
+
+		System.out.println("Enter The Interest Rate : ");
+		interest = Scan.nextDouble();
+
+		System.out.println("Enter The TimePeriod: ");
+		timePeriod = Scan.nextDouble();
+
+		double res = principal * interest * timePeriod / 100;
+
+		System.out.println("The Simple Interest Is " + res);
 
 	}
 
