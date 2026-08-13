df9e408576a7cb8b083de7d308e194570859c9cc
AboutSatyam <programmersatyam07@gmail.com>
2026-08-13 21:46:19 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program2/Car.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program2/Car.java
@@ -6,11 +6,53 @@ public class Car {
 	String model;
 	int year;
 
-	public static void main(String[] args) {
+	void disp() {
+		System.out.println(make);
+		System.out.println(model);
+		System.out.println(year);
+	}
 
-		
-		WS
-		System.out.println("Car Showroom ");
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Car ShowRoom: \n");
+
+		Car cr = new Car();
+		cr.setMake("Toyota");
+		cr.setModel("XR");
+		cr.setYear(2018);
+		cr.disp();
+
+		System.out.println(" ");
+
+		Car cr1 = new Car();
+		cr1.setMake("BMW");
+		cr1.setModel("X7");
+		cr1.setYear(2021);
+		cr1.disp();
+
 	}
 
 }
---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program3/SimpleInterest.java
@@ -0,0 +1,9 @@
+package assignment.byTelusko.program3;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+
+	}
+
+}
