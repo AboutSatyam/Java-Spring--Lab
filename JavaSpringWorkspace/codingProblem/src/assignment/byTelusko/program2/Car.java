570ca408e8e17418608b93a2368c1bad45fe9eb5
AboutSatyam <programmersatyam07@gmail.com>
2026-08-13 21:14:45 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program2/Car.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program2/Car.java
@@ -11,28 +11,4 @@ public class Car {
 		System.out.println("Car Showroom ");
 	}
 
-	public String getMake() {
-		return make;
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public int getYear() {
-		return year;
-	}
-
-	public void setMake(String make) {
-		this.make = make;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
 }
