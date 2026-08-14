b1c423c4edb208ff4e70280d982b12c55ca88ac5
AboutSatyam <programmersatyam07@gmail.com>
2026-08-14 22:38:21 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program9/SalaryEmpolyee.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program9/SalaryEmpolyee.java
@@ -8,7 +8,6 @@ public class SalaryEmpolyee {
 
 		int hourRate = 100;
 		int regularHour = 9;
-		double overtimeHour;
 
 		System.out.println("Welcome Daily Wager Calculator ");
 
@@ -17,15 +16,15 @@ public class SalaryEmpolyee {
 		double hourWorked = input.nextDouble();
 
 		if (hourWorked <= 9) {
-			double res = hourWorked * 100;
+			double res = hourWorked * hourRate;
 			System.out.println((int) res);
 		} else if (hourWorked > 9 && hourWorked <= 24) {
 
 			double over = hourWorked - 9;
 
-			double res = 9 * 100;
+			double res = regularHour * hourRate;
 
-			double sal = (over * 100) * 1.5;
+			double sal = (over * hourRate) * 1.5;
 			int total = (int) (res + sal);
 			System.out.println(total);
 		}
