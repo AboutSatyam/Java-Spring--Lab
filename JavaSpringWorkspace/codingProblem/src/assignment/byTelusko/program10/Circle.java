259f0cc2ba55c25aa8c0d13ba87603c97231e0f6
AboutSatyam <programmersatyam07@gmail.com>
2026-08-14 23:15:25 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program10/Circle.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program10/Circle.java
@@ -1,18 +1,36 @@
 package assignment.byTelusko.program10;
 
+import java.util.Scanner;
+
 public class Circle extends Shape {
 
-	@Override
+	double r;
+
 	void Area() {
 
+		double res = Math.PI * Math.pow(r, 2);
+		System.out.println("Area Is  " + res);
 	}
 
 	@Override
 	void Perimeter() {
 
+		double res1 = 2 * (Math.PI * r);
+		System.out.println("Perimeter Is  " + res1);
 	}
 
 	public static void main(String[] args) {
+
+		System.out.println("Welcome Area And Perimeter Calculator ");
+		Scanner input = new Scanner(System.in);
+
+		Circle cr = new Circle();
+
+		System.out.println("Enter The Radius Of Circle : ");
+		cr.r = input.nextDouble();
+
+		cr.Area();
+		cr.Perimeter();
 
 	}
 
--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program10/Rectriangle.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program10/Rectriangle.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class Rectriangle extends Shape {
 
+	double l;
+	double b;
+
 	@Override
 	void Area() {
 
+		double res = l * b;
+		System.out.println("Area Is " + res);
 	}
 
 	@Override
 	void Perimeter() {
-
+		double res1 = 2 * (l + b);
+		System.out.println("Perimeter Is " + res1);
 	}
 
 	public static void main(String[] args) {
@@ -19,11 +25,16 @@ public class Rectriangle extends Shape {
 		System.out.println("Welcome Area And Perimeter Calculator ");
 		Scanner input = new Scanner(System.in);
 
+		Rectriangle rec = new Rectriangle();
+
 		System.out.println("Enter The Length Of Rectriangle : ");
-		double l = input.nextDouble();
+		rec.l = input.nextDouble();
 
 		System.out.println("Enter The Breadth Of Rectriangle : ");
-		double b = input.nextDouble();
+		rec.b = input.nextDouble();
+
+		rec.Area();
+		rec.Perimeter();
 
 	}
 
