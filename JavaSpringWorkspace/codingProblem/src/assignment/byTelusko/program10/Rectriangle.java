93720498a3c71e64b7dfbeafd970d6977edd2a90
AboutSatyam <programmersatyam07@gmail.com>
2026-08-14 22:53:51 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program10/Rectriangle.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program10/Rectriangle.java
@@ -1,5 +1,7 @@
 package assignment.byTelusko.program10;
 
+import java.util.Scanner;
+
 public class Rectriangle extends Shape {
 
 	@Override
@@ -13,6 +15,15 @@ public class Rectriangle extends Shape {
 	}
 
 	public static void main(String[] args) {
+
+		System.out.println("Welcome Area And Perimeter Calculator ");
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter The Length Of Rectriangle : ");
+		double l = input.nextDouble();
+
+		System.out.println("Enter The Breadth Of Rectriangle : ");
+		double b = input.nextDouble();
 
 	}
 
