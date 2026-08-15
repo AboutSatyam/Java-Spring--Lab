0be066b543e31701a5b9ef02d729644758c84360
AboutSatyam <programmersatyam07@gmail.com>
2026-08-15 16:54:32 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program12/Circle.java
@@ -0,0 +1,20 @@
+package assignment.byTelusko.program12;
+
+public class Circle implements Shape {
+
+	public static void main(String[] args) {
+	}
+
+	@Override
+	public void area() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void perimeter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program12/Triangle.java
@@ -0,0 +1,21 @@
+package assignment.byTelusko.program12;
+
+public class Triangle implements Shape {
+
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public void area() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void perimeter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
