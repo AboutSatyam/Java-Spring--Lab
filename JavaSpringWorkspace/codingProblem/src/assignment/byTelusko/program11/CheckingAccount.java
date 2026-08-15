dbb5e377f2f7c258711c01accd745995251db5e5
AboutSatyam <programmersatyam07@gmail.com>
2026-08-15 15:52:55 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program11/CheckingAccount.java
@@ -0,0 +1,23 @@
+package assignment.byTelusko.program11;
+
+public class CheckingAccount extends BankAccount {
+
+	@Override
+	void deposit() {
+
+	}
+
+	@Override
+	void withdrawal() {
+
+	}
+
+	@Override
+	void displayBalance() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program11/SavingsAccount.java
@@ -0,0 +1,24 @@
+package assignment.byTelusko.program11;
+
+public class SavingsAccount extends BankAccount {
+
+	@Override
+	void deposit() {
+
+	}
+
+	@Override
+	void withdrawal() {
+
+	}
+
+	@Override
+	void displayBalance() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
