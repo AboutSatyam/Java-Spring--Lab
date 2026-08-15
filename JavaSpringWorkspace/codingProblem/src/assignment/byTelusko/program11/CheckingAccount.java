2bcd33f280d3bd27283ab8dd2fb7cdd994bd0b14
AboutSatyam <programmersatyam07@gmail.com>
2026-08-15 16:08:54 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program11/CheckingAccount.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program11/CheckingAccount.java
@@ -4,30 +4,18 @@ import java.util.Scanner;
 
 public class CheckingAccount extends BankAccount {
 
-	Scanner input = new Scanner(System.in);
-	double balance = 10000;
-
 	@Override
 	void deposit() {
-		System.out.println("Enter The Deposit Amount : ");
-		double deposit = input.nextDouble();
-		balance += deposit;
-		System.out.println("Depost Sucessful:  " + deposit);
+
 	}
 
 	@Override
 	void withdrawal() {
-		System.out.println("Enter The Withdrawal Amount : ");
-		double Withdrawal = input.nextDouble();
-		balance -= Withdrawal;
-		System.out.println("Withdrawal Sucessful : " + Withdrawal);
 
 	}
 
 	@Override
 	void displayBalance() {
-		
-		System.out.println("Your Current Balance Is : "+ balance);
 
 	}
 
