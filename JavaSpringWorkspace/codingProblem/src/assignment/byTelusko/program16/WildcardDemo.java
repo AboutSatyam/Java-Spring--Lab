db8179436da495b36315e7ef601125b6cb959950
AboutSatyam <programmersatyam07@gmail.com>
2026-08-16 22:05:00 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program16/WildcardDemo.java
@@ -0,0 +1,14 @@
+package assignment.byTelusko.program16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WildcardDemo {
+
+	List<?> li = new ArrayList<>();
+
+	public static void main(String[] args) {
+
+	}
+
+}
