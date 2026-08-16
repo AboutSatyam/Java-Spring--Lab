8d617bca9849839eaacd841b483471967a00ac34
AboutSatyam <programmersatyam07@gmail.com>
2026-08-16 22:23:33 +0530

    Update

--- JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program16/WildcardDemo.java
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program16/WildcardDemo.java
@@ -1,5 +1,6 @@
 package assignment.byTelusko.program16;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardDemo {
@@ -13,6 +14,25 @@ public class WildcardDemo {
 	}
 
 	public static void main(String[] args) {
+
+		List<Integer> l1 = new ArrayList<>();
+		List<String> l2 = new ArrayList<>();
+		List<Double> l3 = new ArrayList<>();
+
+		WildcardDemo wd = new WildcardDemo();
+
+		l1.add(10);
+		l1.add(20);
+
+		l2.add("Satyam");
+		l2.add("Java");
+
+		l3.add(10.5);
+		l3.add(20.5);
+
+		wd.displayList(l1);
+		wd.displayList(l2);
+		wd.displayList(l3);
 
 	}
 
