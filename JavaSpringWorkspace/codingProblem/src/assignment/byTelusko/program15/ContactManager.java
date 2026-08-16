55b9dcc0e6255140d8e17c59f74772d4348d00e0
AboutSatyam <programmersatyam07@gmail.com>
2026-08-16 21:49:17 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program15/ContactManager.java
@@ -0,0 +1,113 @@
+package assignment.byTelusko.program15;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+public class ContactManager {
+
+	HashMap<Integer, Contact> hm = new HashMap<>();
+
+	void addStudent() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter Your Student ID : ");
+		int id = input.nextInt();
+
+		System.out.println("Enter Your Student Name : ");
+		String name = input.next();
+
+		System.out.println("Enter Your Student Age : ");
+		int age = input.nextInt();
+
+		System.out.println("Enter Your Student Grade : ");
+		String grade = input.next();
+
+		Student student = new Student(name, age, grade);
+
+		hm.put(id, student);
+
+		System.out.println("Student Added Successfully\n");
+	}
+
+	void displayStudent() {
+
+		for (Map.Entry<Integer, Student> entry : hm.entrySet()) {
+
+			System.out.println("Student ID : " + entry.getKey());
+
+			Student student = entry.getValue();
+
+			System.out.println("Name : " + student.name);
+			System.out.println("Age : " + student.age);
+			System.out.println("Grade : " + student.grade + "\n");
+		}
+	}
+
+	void updateStudent() {
+
+		System.out.println("Enter Your Student ID :");
+
+		Scanner input = new Scanner(System.in);
+		int id = input.nextInt();
+
+		if (hm.containsKey(id)) {
+
+			System.out.println("Enter Your Student Name : ");
+			String name = input.next();
+
+			System.out.println("Enter Your Student Age : ");
+			int age = input.nextInt();
+
+			System.out.println("Enter Your Student Grade : ");
+			String grade = input.next();
+
+			Student student = new Student(name, age, grade);
+
+			hm.put(id, student);
+
+			System.out.println("Student Updated Successfully\n");
+
+		} else {
+
+			System.out.println("ID NOT FOUND !!!\n");
+		}
+	}
+
+	void removeStudent() {
+
+		System.out.println("Enter Your Student ID :");
+
+		Scanner input = new Scanner(System.in);
+		int id = input.nextInt();
+
+		if (hm.containsKey(id)) {
+
+			hm.remove(id);
+
+			System.out.println("Student Removed Successfully\n");
+
+		} else {
+
+			System.out.println("ID NOT FOUND !!!\n");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		ContactManager sm = new ContactManager();
+
+		sm.addStudent();
+		sm.displayStudent();
+		sm.updateStudent();
+		sm.displayStudent();
+		sm.removeStudent();
+		sm.displayStudent();
+
+	}
+
+}
+
+	
+
+}
