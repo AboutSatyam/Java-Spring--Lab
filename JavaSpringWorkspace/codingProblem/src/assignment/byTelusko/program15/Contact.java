50e2e1a44c6a56b1f6f562ea571f19fc917e750b
AboutSatyam <programmersatyam07@gmail.com>
2026-08-16 21:33:26 +0530

    Update

---
+++ JavaSpringWorkspace/codingProblem/src/assignment/byTelusko/program15/Contact.java
@@ -0,0 +1,22 @@
+package assignment.byTelusko.program15;
+
+import java.util.HashMap;
+import java.util.HashMap;
+
+public class Contact {
+
+
+	String name;
+	String age;
+	String grade;
+
+	public  Contact(String name, String age, String grade) {
+			super();
+			this.name = name;
+			this.age = age;
+			this.grade = grade;
+		}
+
+}
+
+}
